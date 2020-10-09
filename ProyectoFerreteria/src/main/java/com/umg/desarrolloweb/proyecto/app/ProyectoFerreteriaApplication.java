f5575bb62edf32f434f56a479011759e206e14a3
SEBAS-PC <SEBAS-PC@SEBAS-PC>
2020-10-09 11:18:30 -0600

    comit inicial con eclipse

---
+++ ProyectoFerreteria/src/main/java/com/umg/desarrolloweb/proyecto/app/ProyectoFerreteriaApplication.java
@@ -0,0 +1,13 @@
+package com.umg.desarrolloweb.proyecto.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFerreteriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFerreteriaApplication.class, args);
+	}
+
+}
